72d479ddfbc729ecbd2b49e3eae5420054e0c1b7
beingrajput7233 <rajputashu7200@gmail.com>
2022-09-09 17:54:05 +0530

    phir se change

--- add.cpp
+++ add.cpp
@@ -4,6 +4,8 @@ int main(){
     int a,b;
     cout<<"Enter a and b\n";
     cin>>a>>b;
-    int res=a+b;
-    cout<<res<<endl;
+    //int res=a+b;
+    //cout<<res<<endl;
+    //AB CHANGE KR DIYA
+    cout<<a+b<<endl;
 }
